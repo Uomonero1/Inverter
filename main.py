4c3a107c3d26761660ae2761f579c220bb4b4121
Uomonero1 <niks9498@gmail.com>
2022-06-24 12:42:52 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+a = input("Type something: ").strip()
+char_list = [x for x in a]
+x = 0
+y = -1
+
+while x < len(a)//2 and y < len(a)//2:
+    char_list[x], char_list[y] = char_list[y], char_list[x]
+    x += 1
+    y -= 1
+        
+
+output = "".join(char_list)
+print(output)
